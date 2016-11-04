04336401c2b110690abd7179f0e088255d7f349a
Lesterpig <git@lesterpig.com>
2016-11-04 10:39:12 +0100

    Add first files and configure CI

---
+++ db/drivers/rocksdb/driver.go
@@ -0,0 +1,68 @@
+package rocksdb
+
+import (
+	"errors"
+	"sync"
+
+	"gitlab.com/SporeDB/sporedb/db/version"
+
+	"github.com/tecbot/gorocksdb"
+)
+
+var ro = gorocksdb.NewDefaultReadOptions()
+var wo = gorocksdb.NewDefaultWriteOptions()
+
+var errNotFound = errors.New("key corrupted or unknown")
+
+// S is the driver for the RocksDB store engine.
+type S struct {
+	sync.Mutex
+
+	db *gorocksdb.DB
+}
+
+// New generates a new RocksDB store from the storage path.
+func New(path string) (s *S, err error) {
+	s = &S{}
+
+	opts := gorocksdb.NewDefaultOptions()
+	opts.SetCreateIfMissing(true)
+	s.db, err = gorocksdb.OpenDb(opts, path)
+	return
+}
+
+// Get returns the value and the version stored currently for the specified key.
+func (s *S) Get(key string) (value []byte, v *version.V, err error) {
+	data, err := s.db.Get(ro, []byte(key))
+	if err != nil {
+		return
+	}
+
+	if data.Size() < version.VersionBytes {
+		err = errNotFound
+		v = version.NoVersion
+		return
+	}
+
+	value = data.Data()[version.VersionBytes:]
+	v = &version.V{}
+	err = v.UnmarshalBinary(data.Data()[:version.VersionBytes])
+
+	return
+}
+
+// Set sets the value and the version that must be stored for the specified key.
+func (s *S) Set(key string, value []byte, v *version.V) error {
+	rawVersion, err := v.MarshalBinary()
+	if err != nil {
+		return nil
+	}
+
+	return s.db.Put(wo, []byte(key), append(rawVersion[:version.VersionBytes], value...))
+}
+
+// Close should be used after using the RocksDB store.
+func (s *S) Close() error {
+	s.db.Close()
+	return nil
+}
---
+++ db/store.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"sync"
+
+	"gitlab.com/SporeDB/sporedb/db/drivers/rocksdb"
+	"gitlab.com/SporeDB/sporedb/db/version"
+)
+
+// Store is the interface internal drivers must implement.
+type Store interface {
+	sync.Locker
+	io.Closer
+	// Get returns the value and the version stored currently for the specified key.
+	Get(key string) (value []byte, version *version.V, err error)
+	// Set sets the value and the version that must be stored for the specified key.
+	Set(key string, value []byte, version *version.V) error
+}
+
+// GetTestStore returns a default store for testing purposes.
+// The done return value must be called after running the tests to clean temporary files.
+func GetTestStore() (s Store, done func(), err error) {
+	path, err := ioutil.TempDir("", "rocksdb_")
+	if err != nil {
+		return
+	}
+
+	s, err = rocksdb.New(path)
+	done = func() {
+		if s != nil {
+			_ = s.Close()
+		}
+		_ = os.RemoveAll(path)
+	}
+
+	return
+}
